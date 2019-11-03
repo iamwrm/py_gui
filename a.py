b25b59cfebbbde618e8926c96b4169f6b9777ff2
Ren Wang <iamwrm@gmail.com>
2019-11-02 21:26:08 -0400

    add buttons

--- a.py
+++ a.py
@@ -15,13 +15,35 @@ def exe_cmd_wr(command, *args):
         pass
 
 
+text_input_def = 'You can either input your own tasks or press the button above to get random number'
+
+drop_down_button_tuple = ('Select Algorithm', 'Round Robin',
+                          'First Come First Serve', 'Shortest Job First')
+
+dropdown_algorithms = sg.Drop(values=drop_down_button_tuple, auto_size_text=True, size=(
+    20, 1))
+
+text_tasks_input = sg.Multiline(default_text=text_input_def,
+                                size=(100, 3), key="Tasks Input")
+
+
+str_button_rand = 'Generate Random Tasks'
+str_button_vis = 'Schedule and Visualize'
+
+button_rand_gen = sg.Button(str_button_rand, font=(
+    "Helvetica", 10), button_color=('white', 'green'))
+
+button_visulize = sg.Button(str_button_vis,font=(
+    "Helvetica", 15), size=(20, 3),button_color=('white','orange'))
+
+
+
 layout = [[sg.Text('Scheduling Alogrithm Visualization Tool')],
-          [sg.Button('Generate Random Tasks', font=("Helvetica", 10))],
-          [sg.Multiline(
-              default_text='This is the default Text should you decide not to type anything', size=(35, 3), key="Tasks Input")],
-          [sg.Button('Read'), sg.Exit()],
-          [sg.Button('Touch'), sg.Button('Clear')],
-          [sg.Drop(values=('BatchNorm', 'other'), auto_size_text=True)],
+          [button_rand_gen, ],
+          [text_tasks_input],
+          [dropdown_algorithms, button_visulize],
+          [sg.Button('Touch'), sg.Button('Clear'),
+           sg.Button('Read'), sg.Exit()],
           ]
 
 window = sg.Window('Window that stays open', layout)
@@ -34,9 +56,8 @@ while True:
     if event in (None, 'Exit'):
         break
     if event == 'Generate Random Tasks':
-        keys_entered="GRT"
+        keys_entered = "(5,2,1), (2,1,1), (4,32,1)"
         window.Element('Tasks Input').Update(keys_entered)
-        print(values)
     if event == 'Touch':
         exe_cmd_wr('echo touched')
     if event == 'Clear':
