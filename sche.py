71616237ffe9f1ebc5f4686cc0df9aa0b97997ee
Ren Wang <iamwrm@gmail.com>
2019-11-03 14:17:15 -0500

    some bugs in fifo

--- sche.py
+++ sche.py
@@ -41,6 +41,7 @@ def read_tasks(algo_map_input):
 
     return algo_num, task_tuple_list
 
+
 def fifo(task_list):
     sched_tasks = []
     tick = 0
@@ -61,13 +62,16 @@ def fifo(task_list):
             sched_start = arrive_time - diff
             sched_end = arrive_time + task_length - diff
             tick = sched_end
-        sched_tasks.append(tuple([sched_start,sched_end]))
+        sched_tasks.append(tuple([sched_start, sched_end]))
     return sched_tasks
+
 
 def rr(task_list):
     print(task_list)
     sched_tasks = []
     max_fac_time = 8
+
+    tick = 0
 
     time_left = []
     arr_time = []
@@ -76,51 +80,75 @@ def rr(task_list):
         time_left.append(task[0])
         arr_time.append(task[1])
 
+    i_max = len(task_list)
+    print(i_max)
 
-    exit(0)
-    sched_tasks = [(2,5)]
-    
+    while True:
+        print("ion whiel")
+
+        finished = False
+
+        runned = False
+        for i in range(i_max):
+            if time_left[i] > 0:
+                if arr_time[i] <= tick:
+                    runned = True
+                    # can schedule
+                    if time_left[i] < max_fac_time:
+                        sched_tasks.append(tuple([tick, tick+time_left[i]]))
+                        time_left[i] = 0
+                        tick += time_left[i]
+                    else:
+                        sched_tasks.append(tuple([tick, tick+max_fac_time]))
+                        time_left[i] -= max_fac_time
+                        tick += time_left[i]
+
+        if runned == False:
+            for i in range(i_max):
+                if tick < arr_time[i]:
+                    tick = arr_time[i]
+                    break
+
+            if sum(time_left) == 0:
+                break
+
+        runned = False
+
+    print(sched_tasks)
 
     return sched_tasks
 
 
-algo_map = {"First Come First Serve": 0,"Round Robin":1}
+algo_map = {"First Come First Serve": 0, "Round Robin": 1}
 
 
 algo_num, task_list = read_tasks(algo_map)
 
 
 print('algo_num', algo_num)
-print(task_list, len(task_list))
 
 task_list.sort(key=lambda tup: tup[1])
 
-print(task_list)
 
 sched_tasks = []
 
 if algo_num == 0:
     sched_tasks = fifo(task_list)
 if algo_num == 1:
-    print("Need rrr")
     sched_tasks = rr(task_list)
-    #sched_tasks = fifo(task_list)
-    
+    # sched_tasks = fifo(task_list)
+
 print(sched_tasks)
 
 sche = []
 for index, task in enumerate(sched_tasks):
-    sche.append(tuple([index,task[0],task[1],0]))
-    #print(tuple([index,task[0],task[1],0]))
-
+    sche.append(tuple([index, task[0], task[1], 0]))
+    # print(tuple([index,task[0],task[1],0]))
 
 
 cont = {}
 for i in range(len(sche)):
-    cont[i]={i}
+    cont[i] = {i}
 
 
 draw.draw_canvas(sche, cont, 'a.png')
-
-
-
