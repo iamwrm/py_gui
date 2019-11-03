67a1095e0adc375800b0a559b19032d775338750
Ren Wang <iamwrm@gmail.com>
2019-11-03 10:39:32 -0500

    FIFO schedule

--- a.py
+++ a.py
@@ -15,13 +15,21 @@ def exe_cmd_wr(command, *args):
         pass
 
 
+def print_to_file_wr(file_dir, mode_input, text):
+    with open(file_dir, mode_input) as f:
+        print(text, file=f)
+
+
+text_guide_txt = 'Please input the tasks in the format of (length, arrive time, priority)'
+text_guide = sg.Text(text_guide_txt)
+
 text_input_def = 'You can either input your own tasks or press the button above to get random number'
 
 drop_down_button_tuple = ('Select Algorithm', 'Round Robin',
                           'First Come First Serve', 'Shortest Job First')
 
 dropdown_algorithms = sg.Drop(values=drop_down_button_tuple, auto_size_text=True, size=(
-    20, 1))
+    20, 1),key='dropdown_algo')
 
 text_tasks_input = sg.Multiline(default_text=text_input_def,
                                 size=(100, 3), key="Tasks Input")
@@ -29,23 +37,26 @@ text_tasks_input = sg.Multiline(default_text=text_input_def,
 
 str_button_rand = 'Generate Random Tasks'
 str_button_vis = 'Schedule and Visualize'
+str_button_clear = 'Clear'
 
 button_rand_gen = sg.Button(str_button_rand, font=(
     "Helvetica", 10), button_color=('white', 'green'))
 
-button_visulize = sg.Button(str_button_vis,font=(
-    "Helvetica", 15), size=(20, 3),button_color=('white','orange'))
+button_visulize = sg.Button(str_button_vis, font=(
+    "Helvetica", 15), size=(20, 3), button_color=('white', 'orange'))
+
+button_clear = sg.Button(str_button_clear)
 
 str_image_result = 'image_result'
-image_sche_result = sg.Image(filename=r'./assets/1.png',key=str_image_result)
+image_sche_result = sg.Image(filename=r'./assets/1.png', key=str_image_result)
 
 layout = [[sg.Text('Scheduling Alogrithm Visualization Tool')],
-          [button_rand_gen, ],
+          [text_guide],
+          [button_rand_gen, button_clear],
           [text_tasks_input],
           [dropdown_algorithms, button_visulize],
           [image_sche_result],
-          [sg.Button('Touch'), sg.Button('Clear'),
-           sg.Button('Read'), sg.Exit()],
+          [sg.Exit()],
           ]
 
 window = sg.Window('Window that stays open', layout)
@@ -62,20 +73,13 @@ while True:
     if event == 'Generate Random Tasks':
         keys_entered = "(5,2,1), (2,1,1), (4,32,1)"
         window.Element('Tasks Input').Update(keys_entered)
-    if event == 'Touch':
-        exe_cmd_wr('echo touched')
     if event == 'Clear':
-        exe_cmd_wr('echo cleared')
+        window.Element('Tasks Input').Update("")
     if event == str_button_vis:
-        if state==1:
-            state=0
-            window.Element(str_image_result).Update('./assets/2.png')
-        else:
-            state = 1
-            window.Element(str_image_result).Update('./s1.png')
-
-
-
+        window.Element(str_image_result).Update('./s1.png')
+        # exe_cmd_wr("")
+        print_to_file_wr('./a_out.txt', 'w+', values['dropdown_algo'])
+        print_to_file_wr('./a_out.txt', 'a+', values['Tasks Input'])
 
 
 window.Close()
---
+++ sche.py
@@ -0,0 +1,77 @@
+
+def read_tasks(algo_map_input):
+    task_list = []
+    algo_num = 0
+    with open('a_out.txt', 'r') as f:
+        algo = f.readline().rstrip()
+        try:
+            algo_num = algo_map_input[algo]
+        except:
+            algo_num = 0
+        tasks = f.readline().rstrip()
+        tasks = tasks.replace(")", ',')
+        tasks = tasks.replace("(", '')
+
+    task_list = tasks.split(',')
+    new_list = []
+    for task in task_list:
+        if len(task) > 0:
+            new_list.append(task)
+    task_list = new_list
+
+    new_list = []
+    for val in task_list:
+        new_list.append(int(val))
+    task_list = new_list
+
+    i = 0
+    cache = []
+    task_tuple_list = []
+    for val in task_list:
+        if i % 3 == 2:
+            cache.append(val)
+            task_tuple_list.append(tuple(cache))
+            cache.clear()
+        else:
+            cache.append(val)
+        i += 1
+
+    return algo_num, task_tuple_list
+
+
+algo_map = {"First Come First Serve": 0}
+
+
+algo_num, task_list = read_tasks(algo_map)
+
+
+print('algo_num', algo_num)
+print(task_list, len(task_list))
+
+task_list.sort(key=lambda tup: tup[1])
+
+
+tick = 0
+
+sched_tasks = []
+for task in task_list:
+    arrive_time = task[1]
+    task_length = task[0]
+
+    diff = arrive_time - tick
+
+    sched_start = 0
+    sched_end = 0
+
+    if diff >= 0:
+        sched_start = arrive_time
+        sched_end = arrive_time+task_length
+        tick = sched_end
+    else:
+        sched_start = arrive_time - diff
+        sched_end = arrive_time + task_length - diff
+        tick = sched_end
+    sched_tasks.append(tuple([sched_start,sched_end]))
+    
+print(sched_tasks)
+
